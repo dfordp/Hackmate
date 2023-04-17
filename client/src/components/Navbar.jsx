ce5bfad8cb82517ebd5966640ba4cab50d5bd896
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-04-17 23:06:06 +0530

    :)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+  const [active, setActive] = useState('home')
+  const onHomeClick = () => {setActive('home'); navigate('/home')}
+  const onEventsClick = () => {setActive('myevents'); navigate('/myevents')}
+  const onMessagesClick = () => {setActive('messages'); navigate('/messages')}
+  const onProfileClick = () => {setActive('myprofile'); navigate('/myprofile')}
   return (
     <div className='grid grid-flow-col justify-center gap-80 content-stretch font-medium mt-3 mb-3'>
-    <button>Home</button>
-    <button>My Events</button>
-    <button>Messages</button>
-    <button>My Profile</button>
+    <button onClick={onHomeClick} >Home</button>
+    <button onClick={onEventsClick} >My Events</button>
+    <button onClick={onMessagesClick} >Messages</button>
+    <button onClick={onProfileClick} >My Profile</button>
     </div>
   )
 }
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,18 +1,20 @@
 import React,{useState} from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom'
-import { Header,Footer, Navbar,Home,Messages,MyEvents,MyProfile } from '../components/Index'
+import { Header,Footer, Navbar, Home,MyEvents,Messages,MyProfile } from '../components'
 
 const Main = () => {
   return (
     <div className='grid grid-flow-row bg-lightModeWhite'>
       <Header />
       <Navbar />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/myevents" element={<MyEvents />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/myprofile" element={<MyProfile />} />
-      </Routes> 
+      <div>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/myevents" element={<MyEvents />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes> 
+      </div>
       <Footer className='justify-items-end' />
     </div>
   )
